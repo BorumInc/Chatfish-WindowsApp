96a6c187eb71f5b4f2ada968752b4a389555ad80
Varun <31084854+Borumer@users.noreply.github.com>
2020-07-23 14:04:09 -0400

    Create new Chat(s) buttons, chat search bar

--- Chatfish.Interface/MainWindow.xaml.cs
+++ Chatfish.Interface/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Xml; 
+using System.Xml;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -26,35 +26,61 @@ namespace Chatfish.Interface
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Random Generator {get; set; }
 
         [DllImport("Kernel32.dll")]
         public static extern bool AttachConsole(int processId);
+        private BooleanToVisibilityConverter hiddenOrVisible;
 
         public MainWindow()
         {
             InitializeComponent();
             AttachConsole(-1);
-            Generator = new Random();
+            hiddenOrVisible = new BooleanToVisibilityConverter();
         }
 
         private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             CollectionView cv = (CollectionView)CollectionViewSource.GetDefaultView(ChatList.ItemsSource);
             // Apply the filter
-            cv.Filter = item => {
+            cv.Filter = item =>
+            {
                 XmlElement readableElement = item as XmlElement;
                 String name = readableElement.GetAttribute("Name");
-                return name.Contains(((TextBox) sender).Text);
+                return name.Contains(((TextBox)sender).Text);
             };
+        }
+
+        private void SearchButton_Click(object sender, MouseButtonEventArgs e)
+        {
+            bool searchBoxIsVisible = MessageSearchBox.Visibility == Visibility.Visible;
+            Visibility searchBoxVisibility = (Visibility)hiddenOrVisible.Convert(!searchBoxIsVisible, typeof(Visibility), null, null);
+            MessageSearchBox.Visibility = searchBoxVisibility;
+            ChatName.Visibility = searchBoxIsVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void SearchButton_Hover(object sender, RoutedEventArgs e)
+        {
+            ((Canvas)sender).Cursor = Cursors.Hand;
+            foreach (Shape item in ((Canvas)sender).Children)
+            {
+                item.Stroke = Brushes.Black;
+            }
+        }
+
+        private void SearchButton_Leave(object sender, RoutedEventArgs e)
+        {
+            foreach (Shape item in ((Canvas)sender).Children)
+            {
+                item.Stroke = Brushes.Gray;
+            }
         }
     }
 
     public class Person
     {
         public String Name { get; set; }
-        public int Age {get; set; }
-        public String Department {get; set; }
+        public int Age { get; set; }
+        public String Department { get; set; }
 
         public static Person InstantiateFromXML(string input)
         {
@@ -62,11 +88,11 @@ namespace Chatfish.Interface
 
             using (StringReader sr = new StringReader(input))
             {
-                return (Person) ser.Deserialize(sr);
+                return (Person)ser.Deserialize(sr);
             }
         }
 
         public override String ToString() => $"Name: {Name}\nAge: {Age}\nDepartment: {Department}";
     }
-  
+
 }
